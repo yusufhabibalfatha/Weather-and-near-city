4c5b2f606dd963c41e4dbfb35ef35fcdd088fd86
Yusuf Habib Alfatha <y.habib.alfatha@gmail.com>
2023-09-05 13:23:07 -0700

    Revert "Test git push"
    This reverts commit 488a98e377c39699fd179bf747e5354c561d2f0e.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
     <div className="font-base overflow-x-hidden w-screen h-screen bg-base_four">
       <div className="min-hp-screen grid grid-rows-[min-content_auto_min-content] pt-8">
         <Header setUserCity={setUserCity}/>
+        {/* <div className="h-screen w-40">
+          <div className="bg-black text-white ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, porro.</div>
+        </div> */}
         <Main userCity={userCity} />
         <Footer />
       </div>
